59897b29d8ca1828119c23ca58be790f75cb86c0
Igormcf <igormaciel82@gmail.com>
2022-07-13 17:30:27 -0300

    Finaliza requisito 02

---
+++ src/database/models/user.js
@@ -0,0 +1,18 @@
+module.exports = (sequelize, DataTypes) => {
+  const UserTable = sequelize.define('User', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+    },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.INTEGER,
+    image: DataTypes.STRING,
+  },
+  {
+    timestamps: false,
+    tableName: 'Users',
+  });
+
+  return UserTable;
+};
